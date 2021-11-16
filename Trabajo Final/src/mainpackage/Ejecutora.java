862f7c58194110f5a646f0ac7a3df54f444ee522
Matias Garcia <matu.garcia@hotmail.com.ar>
2021-11-16 12:55:36 -0300

    Add more use cases in Ejecutora

--- Trabajo Final/src/mainpackage/Ejecutora.java
+++ Trabajo Final/src/mainpackage/Ejecutora.java
@@ -6,11 +6,11 @@ public class Ejecutora {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		Empresa e = new Empresa();
-		
+
 		s.useDelimiter(System.getProperty("line.separator"));
-				
+
 		int op;
-		
+
 		do {
 			System.out.println("TP FINAL");
 			System.out.println("1- Agregar un empleado.");
@@ -18,31 +18,48 @@ public class Ejecutora {
 			System.out.println("3- Agregar una convocatoria.");
 			System.out.println("4- Agregar una inscripcion.");
 			System.out.println("5- Agregar una especializacion.");
-			System.out.println("6- Informar algo de algo.");
-			
+			System.out.println("6- Agregar experiencia a un empleado.");
+			System.out.println("7- Agregar empleados inscriptos.");
+			System.out.println("8- Cerrar una convocatoria.");
+			System.out.println("9- Informar convocatorias.");
+			System.out.println("10- Informar puestos.");
+			System.out.println("11- Informar empleados.");
+			System.out.println("12- Informar inscripciones.");
+			System.out.println("13- Informar especializaciones.");
+			System.out.println("14- Informar empleados jerarquicos.");
+			System.out.println("15- Informar puestos postulados por un empleado.");
+			System.out.println("16- Informar empleado con mas inscripciones.");
+			System.out.println("17- Informar postulantes aprobados.");
+
 			op = s.nextInt();
-			switch(op){
-				case 1:{
-					e.agregarEmpleado();
-				}break;
-				case 2:{
-					//e.agregarPuesto();
-				}break;
-				case 3:{
-					//e.agregarConvocatoria();
-				}break;
-				case 4:{
-					//e.agregarInscripcion();
-				}break;
-				case 5:{
-					//e.agregarEspecializacion();
-				}break;
-				case 6:{
-					//e.informar();
-				}break;
+			switch (op) {
+			case 1: {
+				e.agregarEmpleado();
 			}
-		}while(op != 0);
-		
+				break;
+			case 2: {
+				e.agregarPuesto();
+			}
+				break;
+			case 3: {
+				e.agregarConvocatoria();
+			}
+				break;
+			case 4: {
+				// e.agregarInscripcion();
+			}
+				break;
+			case 5: {
+				// e.agregarEspecializacion();
+			}
+				break;
+			case 6: {
+				// e.informar();
+			}
+				break;
+			}
+		} while (op != 0);
+
 		s.close();
 	}
 }
