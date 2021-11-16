65de0a0d88bf7a5f25d0d8d2ef24d1015f0901c4
Sebastian Martinez <84688005+SebastianMartinezA@users.noreply.github.com>
2021-11-15 22:46:52 -0300

    Inicio clases Convocatoria y Inscripcion

--- Trabajo Final/src/mainpackage/Convocatoria.java
+++ Trabajo Final/src/mainpackage/Convocatoria.java
@@ -1,5 +1,20 @@
 package mainpackage;
 
-public class Convocatoria {
+import java.util.ArrayList;
+import java.util.Hashtable;
 
+public class Convocatoria {
+	
+	private int codigo;
+	private Hashtable<String, Integer>experienciaReq;
+	private Puesto puesto;
+	private ArrayList<Inscripcion>inscripciones;
+	
+	public Convocatoria(int cod, Puesto pues) {
+		codigo = cod;
+		puesto = pues;
+		experienciaReq = new Hashtable<String, Integer>();
+		inscripciones = new ArrayList<Inscripcion>();
+	}
+	
 }
--- Trabajo Final/src/mainpackage/Inscripcion.java
+++ Trabajo Final/src/mainpackage/Inscripcion.java
@@ -1,5 +1,15 @@
 package mainpackage;
 
 public class Inscripcion {
-
+	
+	private int codigo;
+	private Empleado empleado;
+	private Convocatoria convocatoria;
+	
+	public Inscripcion (int cod, Empleado emp, Convocatoria conv) {
+		codigo = cod;
+		empleado = emp;
+		convocatoria = conv;
+	}
+	
 }
